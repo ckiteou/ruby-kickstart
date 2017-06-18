fdf7199f5fc390342d7f01828b719f5401aa392a
Costas Kiteou <ckiteou22@gmail.com>
2017-06-18 23:31:34 +0100

    Solved

--- session3/challenge/3_hashes.rb
+++ session3/challenge/3_hashes.rb
@@ -6,3 +6,9 @@
 #
 # Example:
 # word_count "The dog and the cat" # => {"the" => 2, "dog" => 1, "and" => 1, "cat" => 1}
+
+def word_count(string)
+  words = Hash.new { 0 }
+  string.split.each { |i| words[i.downcase] += 1 }
+  words
+end
